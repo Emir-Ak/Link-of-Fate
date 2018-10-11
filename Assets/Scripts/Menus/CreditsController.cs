a0f2df0e811148b7fbcaec3f09eb7b39f7bf6cd2
CodeCtulhu <antonpirogov@mail.ru>
2018-10-11 21:14:12 +0200

    Made main menu and credit panel with text going down

---
+++ Assets/Scripts/Menus/CreditsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour {
+
+	[SerializeField] GameObject creditPanel;
+	[SerializeField] GameObject textObject;
+
+    RectTransform textTransform;
+	public float lerpSpeed;
+
+	private float textHeight;
+	Vector2 startPos;
+	Vector2 endPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+        textTransform = textObject.GetComponent<RectTransform>();
+        textHeight = textTransform.sizeDelta.y;
+		startPos = new Vector3(0, creditPanel.GetComponent<RectTransform>().sizeDelta.y); ;
+		endPos = new Vector3(0,-textHeight,0);
+        StartCoroutine(Lerp(endPos,20));
+    }
+
+
+    IEnumerator Lerp(Vector3 toPos, float time)
+    {
+        float delta = 0;
+        Vector3 original = textTransform.anchoredPosition;
+        while (delta < time)
+        {
+            delta += Time.deltaTime;
+            textTransform.anchoredPosition = Vector3.Lerp(original, toPos, delta / time);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField] private GameObject achievementPanel;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject OptionsPanel;
+    [SerializeField] private GameObject CreditsPanel;
+
+
+
+
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject StartNewButton;
+
+
+
+    #region AchievementPanel
+    public void OpenAchievementsMenu()
+    {
+        mainMenuPanel.SetActive(false);
+        achievementPanel.SetActive(true);
+    }
+
+    public void CloseAchievementsMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        achievementPanel.SetActive(false);
+    }
+    #endregion
+
+    #region Options
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        mainMenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+    }
+    #endregion
+
+    #region Credits
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        mainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+    #endregion
+
+    public void StartNewGame()
+    {
+        Application.Quit();
+    }
+
+    public void Continue()
+    {
+        Application.Quit();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
