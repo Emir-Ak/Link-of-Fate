ccf77a21f0dd962b7140410e97008b4d3cb4d30b
CodeCtulhu <antonpirogov@mail.ru>
2018-10-12 14:19:48 +0200

    A lot of chang the main menu, started a bit on Loading slots and transitions between scenes and main menu (not animations)
    main menu is 75% done
    To do
    Options
    achievement initialization

--- Assets/Scripts/Menus/CreditsController.cs
+++ Assets/Scripts/Menus/CreditsController.cs
@@ -2,38 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreditsController : MonoBehaviour {
+public class CreditsController : MonoBehaviour
+{
 
-	[SerializeField] GameObject creditPanel;
-	[SerializeField] GameObject textObject;
+    [SerializeField] GameObject creditPanel;
+    [SerializeField] GameObject textObject;
 
     RectTransform textTransform;
-	public float lerpSpeed;
+    public float lerpSpeed;
+    [Range(1.0f, 3.0f)] public float speedMultiplier;
+    float currentMultiplier;
 
-	private float textHeight;
-	Vector2 startPos;
-	Vector2 endPos;
+    private float textHeight;
+    private float creditsPanelHeight;
+    Vector2 startPos;
+    Vector2 endPos;
 
-	// Use this for initialization
-	void Start ()
-	{
+    float startTime;
+    float dis;
+    float disCovered;
+    Vector3 original;
+
+    private void OnEnable()
+    {
+        currentMultiplier = 1;
+
         textTransform = textObject.GetComponent<RectTransform>();
-        textHeight = textTransform.sizeDelta.y;
-		startPos = new Vector3(0, creditPanel.GetComponent<RectTransform>().sizeDelta.y); ;
-		endPos = new Vector3(0,-textHeight,0);
-        StartCoroutine(Lerp(endPos,20));
+
+        textHeight = textTransform.sizeDelta.y;//Height of the text object
+        creditsPanelHeight = creditPanel.GetComponent<RectTransform>().sizeDelta.y;//height of the panel (top to bottom)
+
+        startPos = new Vector3(0, creditsPanelHeight);
+        endPos = new Vector3(0, -textHeight, 0);
+
+        textTransform.anchoredPosition = startPos;
+
+        dis = (endPos - startPos).magnitude;
+        original = textTransform.anchoredPosition;
+
+        disCovered = 0;
+
+        StopAllCoroutines();
+        StartCoroutine(Lerp());
     }
 
-
-    IEnumerator Lerp(Vector3 toPos, float time)
+    IEnumerator Lerp()
     {
-        float delta = 0;
-        Vector3 original = textTransform.anchoredPosition;
-        while (delta < time)
+        while (dis > disCovered)
         {
-            delta += Time.deltaTime;
-            textTransform.anchoredPosition = Vector3.Lerp(original, toPos, delta / time);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentMultiplier = speedMultiplier;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                currentMultiplier = 1;
+            }
+
+            disCovered += lerpSpeed * currentMultiplier * Time.deltaTime;
+            textTransform.anchoredPosition = Vector3.Lerp(original, endPos, disCovered / dis);
             yield return null;
         }
     }
 }
+
--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,25 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
     [SerializeField] private GameObject achievementPanel;
     [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private GameObject OptionsPanel;
-    [SerializeField] private GameObject CreditsPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject creditsPanel;
 
-
+    [SerializeField] private GameObject loadingSlotsPanel;
+    [SerializeField] private Image[] slotImages;
 
 
     [SerializeField] private GameObject continueButton;
-    [SerializeField] private GameObject StartNewButton;
+    [SerializeField] private GameObject startNewButton;
 
 
+
+    private void Start()
+    {
+        
+        foreach(Image image in slotImages)
+        {
+            if (image.sprite != null)
+            {
+                continueButton.SetActive(true);
+            }
+        }
+
+        
+    }
 
     #region AchievementPanel
     public void OpenAchievementsMenu()
     {
+        /* This is only temporary code
+         * This is what should be here:
+         * 1. the data is loaded from the achievement saving file
+         * 2. achievements are initialized and created in the content
+        */
         mainMenuPanel.SetActive(false);
         achievementPanel.SetActive(true);
     }
@@ -31,17 +53,31 @@ public class MainMenuController : MonoBehaviour {
     }
     #endregion
 
+    #region Load_Panel
+    public void OpenLoadingSlots()
+    {
+        mainMenuPanel.SetActive(false);
+        loadingSlotsPanel.SetActive(true);
+    }
+
+    public void CloseLoadingSlots()
+    {
+        mainMenuPanel.SetActive(true);
+        loadingSlotsPanel.SetActive(false);
+    }
+    #endregion
+
     #region Options
     public void OpenOptions()
     {
         mainMenuPanel.SetActive(false);
-        OptionsPanel.SetActive(true);
+        optionsPanel.SetActive(true);
     }
 
     public void CloseOptions()
     {
         mainMenuPanel.SetActive(true);
-        OptionsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
     }
     #endregion
 
@@ -49,24 +85,38 @@ public class MainMenuController : MonoBehaviour {
     public void OpenCredits()
     {
         mainMenuPanel.SetActive(false);
-        CreditsPanel.SetActive(true);
+        creditsPanel.SetActive(true);
     }
 
     public void CloseCredits()
     {
         mainMenuPanel.SetActive(true);
-        CreditsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
     #endregion
 
     public void StartNewGame()
     {
-        Application.Quit();
+        /* This is only temporary code
+         * This is what should be here:
+         * 1. Slot menu is opened
+         * 2. In this case if you press on a slot it will start a new game 
+         * (If the slot is occupied permission of overwrite will be asked)
+         * 3. (in the scene from another script) the persistent objects are created and initialized from a default saved values 
+         * (These will be values for new game state)
+        */
+        SceneManager.LoadScene(1);
     }
 
     public void Continue()
     {
-        Application.Quit();
+        /* This is only temporary code
+         * This is what should be here:
+         * 1. the data is loaded according to the last played slot (1,2 or 3)
+         * 2. the scene is changed
+         * 3. (in the scene from another script) the persistent objects are created and initialized
+        */
+        SceneManager.LoadScene(1);
     }
 
     public void ExitGame()
